a6e0fbd7df4b52d74c281d501e5bc74443ae837e
deankago <martokhago@gmail.com>
2025-03-06 14:30:50 +0300

    add registration page add login page add forgot password page add success password reset link page add reset password page add success password reset page

--- src/components/authentication/SuccessfulPassReset.tsx
+++ src/components/authentication/SuccessfulPassReset.tsx
@@ -6,13 +6,19 @@ const SuccessfulPassReset = () => {
     <Main
       footer={
         <p className='w-full text-sm text-[#08040C] mt-4 text-center'>
-          Your password has been reset successfully!
+          <p className='text-sm mt-4'>
+            <a className='underline text-[#1D9B5E] font-[500]' href='/login'>
+              Back to Sign in
+            </a>
+          </p>{' '}
         </p>
       }
     >
       <div className='w-full text-center'>
         <PasswordResetIcon className='w-[60px] h-[60px]' />
-        <h2 className='text-2xl'>Password Reset Link Sent</h2>
+        <h2 className='text-base'>
+          Your password has been reset successfully!
+        </h2>
       </div>
     </Main>
   );
